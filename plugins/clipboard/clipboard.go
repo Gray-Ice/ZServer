1896e2f3dcfde044dbda3aa8d6c77d7f26ed2ecd
Butterfly Smith <57894187+Gray-Ice@users.noreply.github.com>
2023-05-25 22:56:58 +0800

    defined clipboard client

--- plugins/clipboard/clipboard.go
+++ plugins/clipboard/clipboard.go
@@ -21,3 +21,22 @@ func (c *Clipboard) Reset(flag int) {
 func (c *Clipboard) PhoneRequestHandler(ctx *gin.Context) {
 	ctx.JSON(200, gin.H{"text": "This is my clipboard"})
 }
+
+func (c *Clipboard) PhoneURL() string {
+	return "/clipboard/receive"
+}
+
+func (c *Clipboard) PhoneRequestMethod() string {
+	return "post"
+}
+func (c *Clipboard) ClientRequestHandler(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{"text": "I don't know what should I tell you."})
+}
+
+func (c *Clipboard) ClientURL() string {
+	return "/clipboard/client"
+}
+
+func (c *Clipboard) ClientRequestMethod() string {
+	return "get"
+}
